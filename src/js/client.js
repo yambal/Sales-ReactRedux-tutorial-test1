760c43fb9ab10bd100338590b27d9a8a3a288345
ko-he <localhost.ko@gmeil.com>
2017-12-09 17:03:32 +0900

    first react app

--- src/js/client.js
+++ src/js/client.js
@@ -1,2 +1,15 @@
-console.log("hello from webpack")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Layout extends React.Component {
+	render() {
+		return (
+			<h1>Webpack is working!</h1>
+		)
+	}
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<Layout />, app)
 
