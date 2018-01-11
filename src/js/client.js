5f455b464a4dbece523a0a312bc9a3b52d9982b3
ko-he <localhost.ko@gmeil.com>
2018-01-11 19:26:51 +0900

    create first redux application

--- src/js/client.js
+++ src/js/client.js
@@ -1,15 +1,65 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
 
-class Layout extends React.Component {
-	render() {
-		return (
-			<h1>Webpack is working!</h1>
-		)
+const mathReducer = (state = {
+	result: 1,
+	lastValues: []
+},
+action) => {
+	switch (action.type) {
+		case 'ADD':
+			state = {
+				...state,
+				result: state.result + action.value,
+				lastValues: [...state.lastValues, action.value]
+			}
+			break
+		case 'SUBTRACT':
+			state = {
+				...state,
+				result: state.result - action.value,
+				lastValues: [...state.lastValues, action.value]
+			}
+			break
 	}
+
+	return state
 }
 
-const app = document.getElementById('app')
+const userReducer = (state = {
+	name: 'Tiffany',
+	age: 24
+},
+action) => {
+	switch (action.type) {
+		case 'SET_NAME':
+			state = {
+				...state,
+				name: action.name
+			}
+			break
+		case 'SET_AGE':
+			state = {
+				...state,
+				age: action.age
+			}
+			break
+	}
 
-ReactDOM.render(<Layout />, app)
+	return state
+}
 
+const store = createStore(combineReducers({ mathReducer, userReducer}))
+
+store.subscribe(() => {
+	console.log('Store updated', store.getState())
+})
+
+store.dispatch({ type: "ADD", value: 10 })
+
+store.dispatch({ type: "ADD", value: 30 })
+
+store.dispatch({ type: "SUBTRACT", value: 20 })
+
+store.dispatch({ type: "SET_NAME", name: 'backstreat boys' })
+
+store.dispatch({ type: "SET_AGE", age: 20 })
