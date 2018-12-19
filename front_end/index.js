d3d2e9b4b52eb273fc2071d0009526de557b472b
June YAMAMOTO <june@yambal.net>
2018-12-19 18:21:34 +0900

    Api Templete & Asciidoc

--- front_end/index.js
+++ front_end/index.js
@@ -3,4 +3,5 @@ import ReactDom from 'react-dom'
 
 import Auth from './js/Auth.jsx'
 
-ReactDom.render(<Auth />, document.getElementById('app'))
+// ReactDom.render(<Auth />, document.getElementById('app'))
+ReactDom.render(<div>Hello</div>, document.getElementById('app'))
